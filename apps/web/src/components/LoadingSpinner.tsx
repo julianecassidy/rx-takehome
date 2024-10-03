31b7130398c2e3a6e6d69d556b70584516ff832f
Juliane Cassidy <julianecassidy@gmail.com>
2024-10-02 19:20:14 -0600

    add loading, nav, and universal styling

---
+++ apps/web/src/components/LoadingSpinner.tsx
@@ -0,0 +1,9 @@
+export default function LoadingSpinner() {
+    // console.debug("LoadingSpinner");
+
+    return (
+        <div className="LoadingSpinner my-16 flex">
+            <span className="mx-auto loading loading-bars loading-lg"></span>
+        </div>
+    )
+}
